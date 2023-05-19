b2bc6dbb8f9e9809e66787139a55c4dee63b7dc7
Evsey Novikov <daksi.emaill@yandex.ru>
2023-05-19 16:00:53 +0300

    Javadocs

---
+++ Javadocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.example.demo1","l":"H2DatabaseConnection"},{"p":"com.example.demo1","l":"Main"},{"p":"com.example.demo1","l":"Main.Spot"}];updateSearchResults();
